208a6a9e948a868b816229775ea78a3260df338e
DESKTOP-PG8L2MI\Bulle <borderinvais@gmail.com>
2021-07-28 14:40:25 +0300

    -

--- pages/rooms/[id].tsx
+++ pages/rooms/[id].tsx
@@ -3,8 +3,9 @@ import { Header } from "../../components/Header";
 import { useRouter } from "next/router";
 import { Room } from "../../components/Room";
 import { BackButton } from "../../components/BackButton";
+import Axios from "../../core/axios";
 
-const RoomPage:React.FC = ({}) => {
+const RoomPage:React.FC = ({room}) => {
   const route = useRouter();
   const {id} = route.query;
   const obj=[]
@@ -17,9 +18,31 @@ const RoomPage:React.FC = ({}) => {
         fullname="Vlad Burko" />
         <BackButton href="/rooms" />
       </div>
-      <Room title="my first cinversation about..." users={obj}/>
+      <Room title={room.title} users={obj}/>
     </>
   )
 }
 
 export default RoomPage;
+
+export const getServerSideProps = async(ctx) => {
+  try {
+    const {data} = await Axios.get('/rooms.json');
+    const roomId = ctx.query.id;
+    const room = data.find((obj) => obj.id === roomId)
+    return {
+      props: {
+        room: room
+      }
+    };
+  } catch (error) {
+    return {
+      props: {
+        room: []
+      }
+    };
+  }
+  return{
+    props: {},
+  }
+}
